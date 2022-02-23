dbc8e9167b6af05986c5d568e3ac7321d6f82a90
Yugank Pal <87353778+yugankpal@users.noreply.github.com>
2022-02-23 22:38:21 +0530

    Time: 286 ms (61.70%), Space: 118 MB (56.37%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -10,32 +10,60 @@
  */
 class Solution {
 private:
-    bool chek( vector<int>arr){
-        int n = arr.size();
-        int s = 0;
-        int e =n-1;
+   ListNode* getmid(ListNode* head){
+       ListNode* slow = head;
+       ListNode* fast = head->next;
+       
+       while(fast != NULL && fast->next != NULL) {
+           fast = fast->next->next;
+           slow = slow->next;
+       }
+       return slow;
+   }
+    ListNode* reverse(ListNode* head){
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        ListNode* next = NULL;
         
-        while(s<=e){
-            if(arr[s] != arr[e]){
-                return 0;
-            }
-            s++;
-            e--;
+        while(curr != NULL){
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
         }
-        return 1;
-    }
+        return prev;
+        
+    } 
 public:
     bool isPalindrome(ListNode* head) {
-        if(head == NULL ){
-            return 0;
+        if(head == NULL || head->next == NULL){
+            return true;
+                
         }
-        vector<int>arr;
-        ListNode* temp = head;
+        //step 1
+        ListNode* middle = getmid(head);
         
-        while(temp != NULL){
-            arr.push_back(temp->val);
-            temp = temp->next;
+        // step 2
+        ListNode* temp = middle->next;
+        middle->next = reverse(temp);
+        
+        //step 3
+        ListNode* head1 = head;
+        ListNode* head2 = middle->next;
+        
+        while(head2 != NULL){
+            if(head1->val != head2->val){
+                return 0;
+            }
+            head1 = head1->next;
+            head2 = head2->next;
+
         }
-        return chek(arr);
+        
+        temp = middle->next;
+        middle->next = reverse(temp);
+        
+        return 1;
     }
+    
 };
